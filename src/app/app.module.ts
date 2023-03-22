a6c8c8f0d595f2eef644f38b6c3773ffa3770dc7
naledi mmutle <naledi.mmutle@sgits.co.za>
2023-03-22 10:52:28 +0200

    final version commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -10,8 +18,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatIconModule,
+    MatCheckboxModule,
+    HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/service/news.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  api_key = "30727e8f98cb4a49840efe2b6c27f3fd";
+
+  constructor(private http : HttpClient) { }
+
+  initSources() {
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey=' + this.api_key);
+  }
+
+  initArticles() {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' + this.api_key);
+  }
+
+  getArticlesByID(source: String) {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=' + source + '&apiKey=' + this.api_key);
+  }
+}
